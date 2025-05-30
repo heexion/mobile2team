f625f9fc32b646559c63a541cc5fd756924efea1
zsh0819 <zsh1998819@163.com>
2025-05-30 14:48:05 +0900

    복지시설 상세 화면 및 즐겨찾기 기능

--- app/src/main/java/com/example/mobile2team/MainActivity.kt
+++ app/src/main/java/com/example/mobile2team/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.mobile2team.Screen.DetailScreen
 import com.example.mobile2team.ui.theme.Mobile2teamTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Mobile2teamTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                DetailScreen(facilityId = 1L)
             }
         }
     }
